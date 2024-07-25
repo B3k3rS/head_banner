8af9ddd32b7627f5c39ef0cac2e3321da201fac0
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2024-07-25 15:15:39 +0300

    add slider

---
+++ js/index.js
@@ -0,0 +1,12 @@
+let slider = document.querySelector(".head_banner")
+let slides = ["./images/slideshow_1.png","./images/slideshow_2.png"]
+let slideIndex = 0
+
+
+setInterval(() => {
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    slider.style.backgroundImage = `url(${slides[slideIndex]})`
+},5000)
